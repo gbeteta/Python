bdecfe060171a6ce6dbf35b1ec4df9775f358e47
jkhudson80 <50526032+jkhudson80@users.noreply.github.com>
2019-07-22 23:35:50 -0400

    Add files via upload
    Update List
    header.html- Removed search bar
    Team_Members.html- Added roles to each person's name
    app.py- Andres took all the data we were getting from the pandas data reader on each stock and turned it into a graph using bokeh. I added lower year limit of 1990 for the start and end year to prevent errors with the pandas data reader.

--- app.py
+++ app.py
@@ -3,9 +3,10 @@
 
 from flask import Flask, flash, request, render_template, url_for, redirect
 from wtf import DataForm, DateTicker                                    #This is inputing wtf.py that handles the user input form using wt forms
-from flask_wtf import FlaskForm
 from pandas_datareader import data                                      #used to grab stock market data from a website
 import datetime                                                         #used to verify if dates are valid and to be passed to the line above ^
+from bokeh.plotting import figure, show
+
 
 app = Flask(__name__)
 
@@ -111,6 +112,9 @@ def Company_Page(name):
             elif endyear % 100 == 0 and endyear % 400 != 0:
                     message = 'Error: That year does not have a leap day'
                     return render_template('Company_Page.html', name=name, abb=abb, logo=logo, form=form, message=message)
+        elif startyear < 1990 or endyear < 1990:
+            message = 'Error: Enter a year 1990 or later'
+            return render_template('Company_Page.html', name=name, abb=abb, logo=logo, form=form, message=message)
         elif startday < 1 or endday < 1 or monthdaydict[startmonth] < startday or monthdaydict[endmonth] < endday:            #checking that the day enter exists in the month that was entered
             message = 'Error: Enter a valid day number for the month you inputted'
             return render_template('Company_Page.html', name=name, abb=abb, logo=logo, form=form, message=message)
@@ -127,8 +131,35 @@ def Company_Page(name):
 
         start = datetime.datetime(startyear, startmonth, startday)                                                          #start getting stock data at the start date
         end = datetime.datetime(endyear, endmonth, endday)                                                                  #up until the current day
-        AlltheData = data.DataReader(name=abb, data_source="yahoo", start=start, end=end)                                   #***** this bit of code is what grabs the stock data from yahoo *****
-        return render_template('Plot.html', name=name, AlltheData=AlltheData)                                               #displaying new html page at same url that will display the candlestick graph (right now just prints data in raw number form)
+        df = data.DataReader(name=abb, data_source="yahoo", start=start, end=end)                                   #***** this bit of code is what grabs the stock data from yahoo *****
+
+        #Andres's graphing
+        def inc_dec(c, o):
+            if c > o:
+                value = "Increase"
+            elif c < o:
+                value = "Decrease"
+            else:
+                value = "Equal"
+            return value
+        df["Status"] = [inc_dec(c, o) for c, o in zip(df.Close, df.Open)]
+        df["Middle"] = (df.Open + df.Close) / 2
+        df["Height"] = abs(df.Close - df.Open)
+        p = figure(x_axis_type='datetime', width=1600, height=500)
+        abb = companydict[name][0]
+        p.title.text = name.capitalize() + " (" + abb + ")"
+        p.grid.grid_line_alpha = 0.4
+        hours_12 = 12 * 60 * 60 * 1000
+        p.segment(df.index, df.High, df.index, df.Low, color="Black")
+        p.rect(df.index[df.Status == "Increase"], df.Middle[df.Status == "Increase"],
+               hours_12, df.Height[df.Status == "Increase"], fill_color="#CCFFFF", line_color="black")
+        p.rect(df.index[df.Status == "Decrease"], df.Middle[df.Status == "Decrease"],
+               hours_12, df.Height[df.Status == "Decrease"], fill_color="#FF3333", line_color="black")
+        show(p)
+        #End Andres's Graph Stuff
+
+        form = DataForm()
+        return render_template('data.html', form=form)                                               #displaying new html page at same url that will display the candlestick graph (right now just prints data in raw number form)
     return render_template('Company_Page.html', name=name, abb=abb, logo=logo, form=form)                                   #passes the company name, abbreviation, and logo file extension to the html file so that
                                                                                                                                 #the web page can be dynamically rendered with company specifics on it
 
