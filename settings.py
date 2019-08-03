6e39d0c63e9af25fe72f41a47af8df1c7c4e2bc0
gbeteta <gb15d@my.fsu.edu>
2019-08-02 21:31:56 -0400

    Removed unecessary code

--- settings.py
+++
@@ -1,3 +0,0 @@
-def init():
-    global myDict
-    myDict = {'tsla': 'tesla', 'amzn':'amazon', 'aapl':'apple', 'goog':'google', 'sbux':'starbucks', 'nke':'nike', 'msft':'microsoft', 'fb':'facebook', 'xom':'exxonmobil', 'dis':'disney', 'wmt':'walmart', 'v':'visa', 'mcd': 'mcdonalds', 'intc':'intel', 'ntdoy':'nintendo'}
